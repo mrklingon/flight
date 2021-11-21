65b36b9f83e5aff1488f88c30f59e3d896b906d4
Joel Peter Anderson <mrklingon@users.noreply.github.com>
2021-11-21 11:50:40 -0600

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -7,7 +7,7 @@ input.onButtonPressed(Button.A, function () {
 input.onButtonPressed(Button.AB, function () {
     for (let zap = 0; zap <= 4; zap++) {
         led.plot(zap, ALT)
-        landscape[zap] = 0
+        landscape[zap] = ALT - 1
     }
 })
 input.onButtonPressed(Button.B, function () {
@@ -16,39 +16,65 @@ input.onButtonPressed(Button.B, function () {
         ALT = 4
     }
 })
+input.onGesture(Gesture.Shake, function () {
+    pause2 = 1
+    basic.showIcon(IconNames.Heart)
+    basic.showString(convertToText(lives))
+    basic.pause(1000)
+    basic.showIcon(IconNames.Target)
+    basic.showString(convertToText(points))
+    basic.pause(1000)
+    pause2 = 0
+})
 let shifted = 0
 let height = 0
+let pause2 = 0
 let ALT = 0
 let landscape: number[] = []
-let lives = 5
 let points = 0
+let lives = 0
+lives = 5
+points = 0
 landscape = []
 ALT = 0
 for (let index = 0; index < 100; index++) {
-    landscape.unshift(randint(0, 3))
+    landscape.unshift(randint(-3, 3))
 }
+pause2 = 0
 basic.forever(function () {
-    points += 1
-    led.plotBrightness(0, ALT, 255)
-    basic.pause(300)
-    for (let index = 0; index <= 4; index++) {
-        height = landscape[index]
-        for (let index2 = 0; index2 <= 4; index2++) {
-            if (index2 <= height) {
-                led.plotBrightness(index, 4 - index2, 126)
-                if (index == 0 && ALT == 4 - index2) {
-                    lives += -1
+    if (pause2 == 0) {
+        points += 1
+        led.plotBrightness(0, ALT, 255)
+        basic.pause(300)
+        for (let index = 0; index <= 4; index++) {
+            height = Math.abs(landscape[index])
+            for (let index2 = 0; index2 <= 4; index2++) {
+                if (index2 <= height) {
+                    if (0 > landscape[index]) {
+                        led.plotBrightness(index, 4 - index2, 200)
+                    } else {
+                        led.plotBrightness(index, 4 - index2, 50)
+                    }
+                    if (index == 0 && ALT == 4 - index2) {
+                        if (200 == led.pointBrightness(index, 4 - index2)) {
+                            points += 50
+                            lives += 1
+                            basic.showIcon(IconNames.Heart)
+                        } else {
+                            lives += -1
+                        }
+                    }
+                } else {
+                    led.plotBrightness(index, 4 - index2, 0)
                 }
-            } else {
-                led.plotBrightness(index, 4 - index2, 0)
             }
         }
-    }
-    shifted = landscape.shift()
-    landscape.push(shifted)
-    if (lives <= 0) {
-        game.setScore(points)
-        basic.showIcon(IconNames.Sad)
-        game.gameOver()
+        shifted = landscape.shift()
+        landscape.push(randint(-3, 3))
+        if (lives <= 0) {
+            game.setScore(points)
+            basic.showIcon(IconNames.Sad)
+            game.gameOver()
+        }
     }
 })
